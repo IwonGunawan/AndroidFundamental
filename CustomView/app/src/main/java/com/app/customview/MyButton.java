19d66dda9a202652740ff6eefdfb5c004ec1d75a
Iwon Gunawan <iwongunawan@gmail.com>
2020-07-22 17:58:38 +0700

    init custom view

---
+++ CustomView/app/src/main/java/com/app/customview/MyButton.java
@@ -0,0 +1,42 @@
+package com.app.customview;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.drawable.Drawable;
+import android.util.AttributeSet;
+
+import androidx.appcompat.widget.AppCompatButton;
+
+public class MyButton extends AppCompatButton {
+
+    private Drawable enabledBackground, disabledBackground;
+    private int textColor;
+
+
+    public MyButton(Context context) {
+        super(context);
+        init();
+    }
+
+    public MyButton(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        init();
+    }
+
+    public MyButton(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        init();
+    }
+
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+        setBackground(isEnabled() ? enabledBackground : disabledBackground);
+        setTextColor(textColor);
+    }
+
+    private void init() {
+        // I AM HERE
+    }
+}
